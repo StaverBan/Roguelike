6a28a88586b4f8c6208e2b8b8a9ece42c87b8378
StaverBan <44111261+StaverBan@users.noreply.github.com>
2020-05-10 11:26:32 +0300

    Добавил управление персонажем

---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public string rotateTo="R";
+    public Animator playerAnim;
+    public float speed;
+    public Vector2 direction;
+
+    public void Update()
+    {
+      //  direction = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+
+        if (direction.x > 0) rotateTo = "R";
+        if (direction.x < 0) rotateTo = "L";
+        if (direction.sqrMagnitude > 0) playerAnim.Play("Run" + rotateTo);
+        else playerAnim.Play("Stand" + rotateTo);
+
+        transform.Translate(direction*Time.deltaTime* speed);
+    }
+}
---
+++ Assets/Script/VirtualJoystick.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class VirtualJoystick : MonoBehaviour, IEndDragHandler, IBeginDragHandler,IDragHandler
+{
+    public Image Stick;
+    public Image groundStick;
+
+    public PlayerController PC;
+
+    private Vector2 startPosition;
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        groundStick.gameObject.SetActive(true);
+        startPosition = eventData.position;
+        groundStick.transform.position = eventData.position;
+    }
+    public void OnDrag(PointerEventData eventData)
+    {
+        Vector2 nowPosition = eventData.position;
+        Vector2 direction = startPosition - nowPosition;
+        Vector2 normDirection = direction;
+
+        normDirection.Normalize();
+        float magnitude = Mathf.Clamp(direction.magnitude, -50, 50);
+
+        Stick.transform.position = startPosition - normDirection *magnitude ;
+
+        if (Mathf.Abs(magnitude) > 20)
+            PC.direction = -normDirection * magnitude / 50;
+        else
+            PC.direction = Vector2.zero;
+    }
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        groundStick.gameObject.SetActive(false);
+        //Stick.transform.position = eventData.position;
+        PC.direction = Vector2.zero;
+    }
+
+
+}
